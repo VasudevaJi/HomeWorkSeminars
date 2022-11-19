330d11b1955cf1e14ba896b3a505172423771100
Vasu <100xdex@gmail.com>
2022-11-19 07:35:36 +0700

    HomeWork07(1)

---
+++ HomeWork07/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+
+/*
+double[,] CreateRandom2dArray(int rows, int columns)
+{
+    double[,] array = new double[rows, columns];
+
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < columns; j++)
+            array[i,j] =  Math.Round(new Random().NextDouble() + new Random().Next(-10, 11), 4);
+
+    return array;
+}
+
+
+ void Show2dArray(double[,] array)
+ {
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+        
+        Console.WriteLine();
+    }
+ }
+
+
+Console.Write("Input a number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] myArray = CreateRandom2dArray(m, n);
+Show2dArray(myArray);
+*/
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+
+
+
+
