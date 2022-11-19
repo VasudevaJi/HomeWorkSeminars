9d583bf9de7cebb61383b77dc5eea03b9cf2779e
Vasu <100xdex@gmail.com>
2022-11-19 08:04:52 +0700

    HomeWork07(0)

--- HomeWork07/Program.cs
+++ HomeWork07/Program.cs
@@ -36,7 +36,55 @@ Show2dArray(myArray);
 
 // Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 
+/*
+int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, columns];
+
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < columns; j++)
+            array[i,j] = new Random().Next(minValue, maxValue + 1);
+
+    return array;
+}
 
 
+ void Show2dArray(int[,] array)
+ {
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+        
+        Console.WriteLine();
+    }
+ }
+
+void FindIndex(int[,] array, int i, int j)
+{
+    if(i > array.GetLength(0) || j > array.GetLength(1))
+        Console.WriteLine("Array does not have this index");
+    else
+        Console.WriteLine(array[i,j]);
+}
 
 
+Console.Write("Input a number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(m, n, min, max);
+Show2dArray(myArray);
+
+Console.Write("Input a string search: ");
+int rowsSearch = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a columns search: ");
+int colSearch = Convert.ToInt32(Console.ReadLine());
+
+FindIndex(myArray, rowsSearch, colSearch);
+*/
