f75ea7f37af13c3394d154c170008354144bc6aa
Vasu <100xdex@gmail.com>
2022-11-19 16:51:40 +0700

    HomeWork07

--- HomeWork07/Program.cs
+++ HomeWork07/Program.cs
@@ -88,3 +88,71 @@ int colSearch = Convert.ToInt32(Console.ReadLine());
 
 FindIndex(myArray, rowsSearch, colSearch);
 */
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+/*
+int[,] CreateRandom2dArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < columns; j++)
+            array[i,j] = new Random().Next(1, 10);
+
+    return array;
+}
+
+
+ void Show2dArray(int[,] array)
+ {
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + "  ");
+        
+        Console.WriteLine();
+    }
+ }
+
+
+double[] MeanOfColumns(int[,] array)
+ {
+    double[] resArray = new double [array.GetLength(1)]; 
+    for(int i = 0; i < array.GetLength(1); i++)
+    {
+        double sumColumn = 0;
+        for(int j = 0; j < array.GetLength(0); j++)
+            sumColumn += array[j,i];
+        resArray[i] = Math.Round(sumColumn / array.GetLength(1), 3);
+    }
+    return resArray;
+ }
+
+ void ShowArray(double[] array)
+ {
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+            Console.Write(array[i] + "  ");
+        
+    }
+ }
+
+
+
+Console.Write("Input a number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(m, n);
+Show2dArray(myArray);
+
+Console.WriteLine(" ");
+double[] resultArray = MeanOfColumns(myArray);
+Console.WriteLine("Arithmetic mean of each column:");
+ShowArray(resultArray);
+*/
+
+
